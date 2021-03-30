be2da9f7a02e9b24378c216e5a10292e1f2c9886
NASIR_HOSSAIN <nasirxpress12@gmail.com>
2021-03-31 01:01:57 +0600

    Variable

---
+++ Variable/Variable.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main(){
+
+//var name string = "NASIR"
+//var name2 , city string = "NASIR_HOSSAIN", "PABNA"
+//fmt.Println(name , city, name2)
+
+/*
+var name string = "NASIR"
+var name2 , city string = "NASIR_HOSSAIN", "PABNA"
+fmt.Println(name , city, name2)
+*/
+
+//char
+var c rune = 'A'
+var age int = 10
+var result float32 =50.95
+
+fmt.Println(c, age, result)
+
+}
