d5e385314fffc7ad0cebd2fa1e17ed915dcf1618
NASIR_HOSSAIN <nasirxpress12@gmail.com>
2021-03-31 01:03:14 +0600

    Rune_Boolean

---
+++ Rune_Boolean/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main(){
+//var name string
+//ver age int
+//var result float32
+//var isFound bool
+
+//fmt.Println(name)
+//fmt.Println(age)
+//fmt.Println(result)
+
+var chr rune
+chr = 'F'
+fmt.Println(chr)
+fmt.Printf("%c", chr)
+
+//fmt.Println(isFound)
+//fmt.Println(true && true) 
+//fmt.Println(true && false) 
+//fmt.Println(true || ture) 
+//fmt.Println(true || false) 
+//fmt.Println(!true) 
+
+}
